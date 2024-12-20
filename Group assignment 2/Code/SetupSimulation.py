d45161366007bd8d020563926f9071f813570197
BoerTonnie <Tonnie3133@gmail.com>
2024-12-20 14:11:56 +0100

    pjot heeft een neus

---
+++ Group assignment 2/Code/SetupSimulation.py
@@ -0,0 +1,45 @@
+# ------------- Imports ---------------
+import pandas as pd
+import matplotlib as plt
+import math
+
+
+
+
+
+# ------------- Variables & constants -------------
+
+simulatedCycleTime_ms = 50
+
+gravity = 9.81
+startingAngle = 0
+startingDistance = 100
+
+ballRadius = 12.5
+
+rail_length = 250 # length of the rail, accessible to the ball
+sensor_Offset = 40 # distance from sensor to the rail
+
+
+
+
+
+
+# ------------- Functions -------------
+
+class simulate:
+    def newCycle():
+        print("New simulation cycle")
+
+
+
+
+
+# ------------- main cycle -------------
+
+if __name__ == "__main__":
+    simulation = simulate
+    simulation.newCycle()
+
+
+    print(50)
