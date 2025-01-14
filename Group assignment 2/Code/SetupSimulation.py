323ecbf67fc8aa0cb763aaa72ff94f9cee4045d5
BoerTonnie <Tonnie3133@gmail.com>
2025-01-14 11:52:52 +0100

    praised and kicked down the staris

--- Group assignment 2/Code/SetupSimulation.py
+++ Group assignment 2/Code/SetupSimulation.py
@@ -24,13 +24,18 @@ class simulate(gym.Env):
         self.deltaSimulationTime = 0.05  # amount of time between each cycle of simulation
         self.speed = 0
         self.distance = StartingDistance
-        self.simulationTime = 0
+        self.simulationTime = 0 
 
-        self.maxDistance = 30
-        self.minDistance = 4
+        self.maxDistance = 1
+        self.minDistance = -1
         self.goal_threshold = 0.5
 
-        self.maxSimulationTime = 3000 # this number times 0.05 = simTime
+        # GENERATE RANDOM TARGET
+        lowerBound = self.minDistance + self.goal_threshold
+        upperBound = self.maxDistance - self. goal_threshold
+        self.goal = random.uniform(lowerBound, upperBound)
+
+        self.maxSimulationTime = 150 # this number times 0.05 = simTime
 
         self.stepCounter = 0
         self.stepCounterMax = 10
@@ -41,15 +46,16 @@ class simulate(gym.Env):
             'Angle': [0],
             'Acceleration': [0],
             'Velocity': [self.speed],
-            'Distance': [self.distance]
+            'Distance': [self.distance],
+            'Goal': [self.goal]
         }
 
         self.df = pd.DataFrame(data)
         self.action_space = Box(-1, 1, (1,), np.float32) # Action space for: Servo_Angle_Low, Servo_Angle_High, Shape(How much servos there are), data type
         # Define the observation space
         self.observation_space = Box(
-            low=np.array([0.0, -10, -10]),    # Lower bounds for distance, velocity, and angle
-            high=np.array([30, 10, 10]), # Upper bounds for distance, velocity, and angle
+            low=np.array([-1, -1, -1]),    # Lower bounds for distance, velocity, and angle
+            high=np.array([1, 1, 1]), # Upper bounds for distance, velocity, and angle
             dtype=np.float32
         )
 
@@ -65,12 +71,20 @@ class simulate(gym.Env):
     def acceleration(self, v, theta):
         return -9.81 * np.sin((2*theta)/np.pi)
     
-    def mapAngle(self, servoAngle):
-        servo_min = -1
-        servo_max = 1
+    def mapAngle(self, servoAngle, direction):
+        if direction == False:
+            servo_min = -1
+            servo_max = 1
 
-        angle_min = -10
-        angle_max = 10
+            angle_min = -10
+            angle_max = 10
+        else:
+            servo_min = -10
+            servo_max = 10
+
+            angle_min = -1
+            angle_max = 1
+            
 
         return (servoAngle - servo_min) * (angle_max - angle_min) / (servo_max - servo_min) + angle_min
     
@@ -86,7 +100,8 @@ class simulate(gym.Env):
             'Angle': float(angle) if isinstance(angle, (int, float)) else float(angle[0]),
             'Acceleration': float(acc) if isinstance(acc, (int, float)) else float(acc[0]),
             'Velocity': float(speed_scalar),
-            'Distance': float(distance_scalar)
+            'Distance': float(distance_scalar),
+            'Goal': float(self.goal)
         }
         self.df.loc[len(self.df)] = newData
         
@@ -122,18 +137,20 @@ class simulate(gym.Env):
         self.df.to_csv(filename, index=False)
         print(f"Simulation data saved to {filename}")        
 
-    def newCycle(self, servoAngle):
-        #newDistance = self.distance * 
+    def newCycle(self, IMUAngle):
         #print("New simulation cycle")
+        # IMU angle: -1 - > 1
+        # self.angle: -10 -> 10
+        calculatedAngle = self.mapAngle(IMUAngle, direction=False)
 
-        self.angle = self.mapAngle(servoAngle)
-        acc = self.acceleration(self.speed, self.angle)  # calculate acceleration on this cycle
+        self.angle = IMUAngle
+        acc = self.acceleration(self.speed, calculatedAngle)  # calculate acceleration on this cycle
         self.speed = self.speed + acc * self.deltaSimulationTime # update speed
         self.distance = self.distance + self.speed * self.deltaSimulationTime # update distance to sensor
         self.simulationTime = self.simulationTime + self.deltaSimulationTime # itterate time for data storing purposes
 
         # Store information for debugging and plotting purposes
-        self.storeInfo(self.angle, acc)
+        self.storeInfo(calculatedAngle, acc)
 
 
 
@@ -187,10 +204,15 @@ class simulate(gym.Env):
     
     def step(self, action):
         # set the servo angle to action
-        Actual_angle = action # read actual
+        Actual_angle = action # read actual (Should be -1 -> 1)
 
         # do simuloation stuff but later read from arduino
         self.newCycle(Actual_angle)
+        # self.angle (-1 -> 1) represents -10 -> 10 degrees
+        # self.speed (-1 -> 1) represents -10 -> 10 cm/s
+        # self.distance (-1 -> 1) represemts 0 -> 30 cm
+
+
 
         # calculate termination
         terminated = bool(
@@ -202,7 +224,8 @@ class simulate(gym.Env):
         # calcualte reward
         # Question to Hussam: Should we also put the goal somewhere else so the model knows where to aim itself to?
         if not terminated:
-            reward = 0.5
+            reward = float(0.5*(1 - (abs(self.goal - self.distance[-1])) / self.maxDistance))
+
             if self.distance > self.goal + self.goal_threshold and self.distance < self.goal - self.goal_threshold:
                 reward += 0.5
         elif self.steps_beyond_terminated is None:
@@ -284,7 +307,7 @@ if __name__ == "__main__":
 
 
     # Initialize the environment with a starting distance
-    env = simulate(StartingDistance=10)
+    env = simulate(StartingDistance=0.2)
 
     # Wrap the environment with Monitor for logging
     env = Monitor(env)
@@ -307,7 +330,7 @@ if __name__ == "__main__":
     model.save("simulate_ppo_model")
 
     # Evaluate the trained model
-    mean_reward, std_reward = evaluate_policy(model, vec_env, n_eval_episodes=50)
+    mean_reward, std_reward = evaluate_policy(model, vec_env, n_eval_episodes=10)
     print(f"Mean reward: {mean_reward} +/- {std_reward}")
 
 
